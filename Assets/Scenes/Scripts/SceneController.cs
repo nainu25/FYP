7ebcd7d5ba459d5a4acdd7c1f625045586dc6f19
nainu25 <chohanhasnain24@gmail.com>
2024-10-07 01:21:07 +0500

    Main menu done and 2 buttons are functional

---
+++ Assets/Scenes/Scripts/SceneController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneController : MonoBehaviour
+{
+    int sceneIndex;
+    void Start()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            StartCoroutine(EndSplashScreen());
+        }
+    }
+    
+    IEnumerator EndSplashScreen()
+    {
+        yield return new WaitForSeconds(1.9f);
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+        Debug.Log("Quitted");
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
